0908c09755ae665bdfc59b235752dc60dca5530f
Dave Shuckerow <djshuckerow@gmail.com>
2017-02-21 00:26:16 -0400

    Add everything from step 4.

--- lib/badge_component.dart
+++ lib/badge_component.dart
@@ -6,8 +6,21 @@ import 'package:angular2/core.dart';
     styleUrls: const ['badge_component.css'])
 class BadgeComponent {
   String badgeName = 'Sergey';
+  String buttonText = 'Aye! Gimme a name!';
+  bool isButtonEnabled = true;
+
+  void generateBadge() {
+    badgeName = "Davy Jones";
+  }
 
   void updateBadge(String inputName) {
     badgeName = inputName;
+    if (inputName.trim().isEmpty) {
+      buttonText = 'Arrr! Write yer name!';
+      isButtonEnabled = false;
+    } else {
+      buttonText = 'Aye! Gimme a name!';
+      isButtonEnabled = true;
+    }
   }
 }
