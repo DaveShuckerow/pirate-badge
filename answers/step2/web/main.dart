927003968ab476d1128de4cd38d61747a9ec4bcb
Dave Shuckerow <djshuckerow@google.com>
2017-03-05 11:25:22 -0800

    Roll in step 2

---
+++ answers/step2/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular2/platform/browser.dart';
+
+import 'package:pirate_badge/app_component.dart';
+
+main() {
+  bootstrap(AppComponent);
+}
--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,11 +1,6 @@
 import 'package:angular2/core.dart';
 
-import 'badge_component.dart';
-
-@Component(
-    selector: 'my-app',
-    templateUrl: 'app_component.html',
-    directives: const [BadgeComponent])
+@Component(selector: 'my-app', template: '<h1>Hello {{name}}</h1>')
 class AppComponent {
   var name = 'Angular';
 }
